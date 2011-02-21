6e41132cb559579a6f8ffbed88cbbccfa9bf4a28
fabozzi <>
2011-02-21 14:28:09 +0000

    *** empty log message ***

--- test/higgs2l2bAnalysis.py
+++ test/higgs2l2bAnalysis.py
@@ -3,11 +3,15 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("TRIM")
 
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 process.load("HiggsAnalysis.Higgs2l2b.Higgs2l2bedmNtuples_cff")
 #process.load("HiggsAnalysis.Higgs2l2b.zjjEdmNtuples_cff")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.MessageLogger.cerr.threshold = ''
+process.MessageLogger.cerr.FwkReport.reportEvery = 10000
 
 process.TFileService = cms.Service("TFileService", fileName = cms.string('h2l2b300GF.root') )
 
