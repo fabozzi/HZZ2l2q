0bfce645473570252a5f4b6b7002d82ea3552df5
decosa <>
2011-05-10 18:41:24 +0000

    drop provenance - not include emu category

--- test/higgs2l2bNtuple.py
+++ test/higgs2l2bNtuple.py
@@ -7,7 +7,7 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 process.load("HiggsAnalysis.Higgs2l2b.Higgs2l2bedmNtuples_cff")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 #process.MessageLogger.cerr.threshold = ''
 #process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
@@ -16,27 +16,18 @@ process.TFileService = cms.Service("TFileService", fileName = cms.string('h2l2b4
 process.source = cms.Source("PoolSource")
 
 process.source.fileNames=cms.untracked.vstring(
-    'rfio:/dpm/na.infn.it/home/cms/store/user/fabozzi/Run2011A/Skim/DoubleMu/160404-161352_v2/h2l2bData_10_1_0WE.root',
-    'rfio:/dpm/na.infn.it/home/cms/store/user/fabozzi/Run2011A/Skim/DoubleMu/160404-161352_v2/h2l2bData_11_1_fgJ.root',
-    'rfio:/dpm/na.infn.it/home/cms/store/user/fabozzi/Run2011A/Skim/DoubleMu/160404-161352_v2/h2l2bData_12_1_M8d.root',
-    'rfio:/dpm/na.infn.it/home/cms/store/user/fabozzi/Run2011A/Skim/DoubleMu/160404-161352_v2/h2l2bData_1_1_Lxw.root',
-    'rfio:/dpm/na.infn.it/home/cms/store/user/fabozzi/Run2011A/Skim/DoubleMu/160404-161352_v2/h2l2bData_2_1_yjl.root',
-    'rfio:/dpm/na.infn.it/home/cms/store/user/fabozzi/Run2011A/Skim/DoubleMu/160404-161352_v2/h2l2bData_3_1_G8v.root',
-    'rfio:/dpm/na.infn.it/home/cms/store/user/fabozzi/Run2011A/Skim/DoubleMu/160404-161352_v2/h2l2bData_4_1_LRg.root',
-    'rfio:/dpm/na.infn.it/home/cms/store/user/fabozzi/Run2011A/Skim/DoubleMu/160404-161352_v2/h2l2bData_5_1_sxk.root',
-    'rfio:/dpm/na.infn.it/home/cms/store/user/fabozzi/Run2011A/Skim/DoubleMu/160404-161352_v2/h2l2bData_6_1_seh.root',
-    'rfio:/dpm/na.infn.it/home/cms/store/user/fabozzi/Run2011A/Skim/DoubleMu/160404-161352_v2/h2l2bData_7_1_l3b.root',
-    'rfio:/dpm/na.infn.it/home/cms/store/user/fabozzi/Run2011A/Skim/DoubleMu/160404-161352_v2/h2l2bData_8_1_Ukd.root',
-    'rfio:/dpm/na.infn.it/home/cms/store/user/fabozzi/Run2011A/Skim/DoubleMu/160404-161352_v2/h2l2bData_9_1_sbR.root',
+    'file:/data3/scratch/users/decosa/Higgs/Z0Jets/skim/h2l2bData_10_1_RX7.root'
 )
 
-process.edmNtuplesOut.fileName = cms.untracked.string('ntupleMu/h2l2b_ntuple.root')
+process.edmNtuplesOut.fileName = cms.untracked.string('ntupleMu/h2l2b_ntuple_old_withemu.root')
+process.edmNtuplesOut.dropMetaData = cms.untracked.string('ALL')
+
 
 process.analysisPath = cms.Path(
     process.eventVtxInfoNtuple+
     process.Higgs2e2bEdmNtuple+
-    process.Higgs2mu2bEdmNtuple+
-    process.Higgsemu2bEdmNtuple
+    process.Higgs2mu2bEdmNtuple
+#    process.Higgsemu2bEdmNtuple
 )
 
 process.endPath = cms.EndPath(process.edmNtuplesOut)
