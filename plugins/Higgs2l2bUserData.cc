7bbce2dfafd5f7f050ca4ee36ac1c6848d357b76
fabozzi <>
2011-05-30 15:31:42 +0000

    *** empty log message ***

--- plugins/Higgs2l2bUserData.cc
+++ plugins/Higgs2l2bUserData.cc
@@ -19,10 +19,10 @@
 
 // Helicity angles 
 #include "HiggsAnalysis/Higgs2l2b/interface/Helicity.h"
-// Likelihood discriminant
-#include "HiggsAnalysis/Higgs2l2b/interface/HelicityLikelihoodDiscriminant.h"
 // KinFit
 #include "HiggsAnalysis/Higgs2l2b/interface/JetKinFitter.h"
+// Likelihood discriminant
+#include "HiggsAnalysis/Higgs2l2b/interface/HelicityLikelihoodDiscriminant.h"
 
 #include <Math/VectorUtil.h>
 #include <vector>
@@ -38,18 +38,18 @@ public:
 private:
   void produce( edm::Event &, const edm::EventSetup & );
   void helicityAngles(const pat::CompositeCandidate &, const reco::Candidate *, const reco::Candidate *);
-  //  int runJetKinFit(TLorentzVector &, TLorentzVector &, 
-  //		   const TLorentzVector &, TLorentzVector &, TLorentzVector &,
-  //		   float &, float &);
+  int runJetKinFit(TLorentzVector &, TLorentzVector &, 
+  		   const TLorentzVector &, TLorentzVector &, TLorentzVector &,
+  		   float &, float &);
   
   InputTag higgsTag, gensTag, metTag;
   PFJetIDSelectionFunctor jetIDLoose;
   pat::strbitset ret; 
   double costhetaNT1, costhetaNT2, costhetastarNT, phiNT,
     phistarNT1, phistarNT2, phistarNT12, phiNT1, phiNT2;
+  double zNominalMass_;
+  JetKinFitter kinFitter_;
   //  HelicityLikelihoodDiscriminant LD_;
-  //  double zNominalMass_;
-  //  JetKinFitter kinFitter_;
 };
 
 Higgs2l2bUserData::Higgs2l2bUserData( const ParameterSet & cfg ):
@@ -57,9 +57,9 @@ Higgs2l2bUserData::Higgs2l2bUserData( const ParameterSet & cfg ):
   gensTag( cfg.getParameter<edm::InputTag>("gensTag")),
   metTag( cfg.getParameter<edm::InputTag>("metTag")),
   jetIDLoose( PFJetIDSelectionFunctor::FIRSTDATA,
-	      PFJetIDSelectionFunctor::LOOSE )
-				    //  zNominalMass_( 91.1876 ),
-				    //  kinFitter_( zNominalMass_, 0.0 )
+	      PFJetIDSelectionFunctor::LOOSE ),
+  zNominalMass_( 91.1876 ),
+  kinFitter_( zNominalMass_, 0.0 )
 {
   ret = jetIDLoose.getBitTemplate();
   produces<vector<pat::CompositeCandidate> >("h").setBranchAlias( "h" );
@@ -89,17 +89,17 @@ void Higgs2l2bUserData::produce( Event & evt, const EventSetup & ) {
   float j0LooseID, j1LooseID;
   bool  jminbmatch, jmincmatch, jmaxbmatch, jmaxcmatch;
   float  lminpt, lmineta, lminphi, lmaxpt, lmaxeta, lmaxphi, jminpt, jmineta, jminphi, jmaxpt,  jmaxeta,  jmaxphi;
+  float j1RefitPt, j2RefitPt;
+  float j1RefitEta, j2RefitEta;
+  float j1RefitPhi, j2RefitPhi;
+  float j1RefitE, j2RefitE;
+  float ZjjRefitMass;
+  float HZZRefitMass;
+  float KFchiSquare, KFchiSquareProb;
+  TLorentzVector j1corr;
+  TLorentzVector j2corr;
+  TLorentzVector HZZKinFit4mom, ZLL4mom, Zjj4mom; //initialized to (0, 0, 0 ,0)
   //  float helyLD;
-  //  float j1RefitPt, j2RefitPt;
-  //  float j1RefitEta, j2RefitEta;
-  //  float j1RefitPhi, j2RefitPhi;
-  //  float j1RefitE, j2RefitE;
-  //  float ZjjRefitMass;
-  //  float HZZRefitMass;
-  //  float KFchiSquare, KFchiSquareProb;
-  //  TLorentzVector j1corr;
-  //  TLorentzVector j2corr;
-  //  TLorentzVector HZZKinFit4mom, ZLL4mom, Zjj4mom; //initialized to (0, 0, 0 ,0)
 
   for (unsigned int i = 0; i< higgsH->size();++i){
     const reco::CompositeCandidate & H = (*higgsH)[i];
@@ -217,57 +217,57 @@ void Higgs2l2bUserData::produce( Event & evt, const EventSetup & ) {
     
 
     // prepare input for KinFit
-    //    double j1en = j1.energy(); 
-    //    double j1pt = j1.pt();
-    //    double j1eta = j1.eta(); 
-    //    double j1phi = j1.phi();
-    //    double j2en = j2.energy(); 
-    //    double j2pt = j2.pt(); 
-    //    double j2eta = j2.eta(); 
-    //    double j2phi = j2.phi();
-    //    j1corr.SetPtEtaPhiE(j1pt,j1eta,j1phi,j1en);
-    //    j2corr.SetPtEtaPhiE(j2pt,j2eta,j2phi,j2en);
-    //    ZLL4mom.SetPtEtaPhiM(Zll->pt(), Zll->eta(), Zll->phi(), Zll->mass());
+    double j1en = j0.energy(); 
+    double j1pt = j0.pt();
+    double j1eta = j0.eta(); 
+    double j1phi = j0.phi();
+    double j2en = j1.energy(); 
+    double j2pt = j1.pt(); 
+    double j2eta = j1.eta(); 
+    double j2phi = j1.phi();
+    j1corr.SetPtEtaPhiE(j1pt,j1eta,j1phi,j1en);
+    j2corr.SetPtEtaPhiE(j2pt,j2eta,j2phi,j2en);
+    ZLL4mom.SetPtEtaPhiM(Zll->pt(), Zll->eta(), Zll->phi(), Zll->mass());
 
     // run KinFit
-    //    int kinfitstatus = runJetKinFit(j1corr, j2corr, ZLL4mom, Zjj4mom, HZZKinFit4mom, KFchiSquare, KFchiSquareProb);
+    int kinfitstatus = runJetKinFit(j1corr, j2corr, ZLL4mom, Zjj4mom, HZZKinFit4mom, KFchiSquare, KFchiSquareProb);
 
-    //    if (kinfitstatus==0) {
-    //      j1RefitPt = j1corr.Pt();
-    //      j2RefitPt = j2corr.Pt();
-    //      j1RefitEta = j1corr.Eta();
-    //      j2RefitEta = j2corr.Eta();
-    //      j1RefitPhi = j1corr.Phi(); 
-    //      j2RefitPhi = j2corr.Phi();
-    //      j1RefitE = j1corr.E(); 
-    //      j2RefitE = j1corr.E();
-    //      ZjjRefitMass = Zjj4mom.M();
-    //      HZZRefitMass = HZZKinFit4mom.M();
-    //    } else {
-    //      //kinematic fit failed
-    //      j1RefitPt = 0;
-    //      j2RefitPt = 0;
-    //      j1RefitEta = 0;
-    //      j2RefitEta = 0;
-    //      j1RefitPhi = 0; 
-    //      j2RefitPhi = 0;
-    //      j1RefitE = 0; 
-    //      j2RefitE = 0;
-    //      ZjjRefitMass = 0;
-    //      HZZRefitMass = 0;
-    //      KFchiSquare = -1. ; 
-    //      KFchiSquareProb = -1.;
-    //    }
+    if (kinfitstatus==0) {
+      j1RefitPt = j1corr.Pt();
+      j2RefitPt = j2corr.Pt();
+      j1RefitEta = j1corr.Eta();
+      j2RefitEta = j2corr.Eta();
+      j1RefitPhi = j1corr.Phi(); 
+      j2RefitPhi = j2corr.Phi();
+      j1RefitE = j1corr.E(); 
+      j2RefitE = j2corr.E();
+      ZjjRefitMass = Zjj4mom.M();
+      HZZRefitMass = HZZKinFit4mom.M();
+    } else {
+      //kinematic fit failed
+      j1RefitPt = 0;
+      j2RefitPt = 0;
+      j1RefitEta = 0;
+      j2RefitEta = 0;
+      j1RefitPhi = 0; 
+      j2RefitPhi = 0;
+      j1RefitE = 0; 
+      j2RefitE = 0;
+      ZjjRefitMass = 0;
+      HZZRefitMass = 0;
+      KFchiSquare = -1. ; 
+      KFchiSquareProb = -1.;
+    }
 
     // Get Helicity angles (to check!!!!)
-    //    HelicityLikelihoodDiscriminant::HelicityAngles myha;
+    // HelicityLikelihoodDiscriminant::HelicityAngles myha;
     //    myha.helCosTheta1    = H.userFloat("costhetaNT1");
     //    myha.helCosTheta2    = H.userFloat("costhetaNT2");
     //    myha.helCosThetaStar = H.userFloat("costhetastarNT");
     //    myha.helPhi          = H.userFloat("phiNT");
     //    myha.helPhi1         = H.userFloat("phiNT1");
     //    myha.mzz             = H.mass();
-    //    if (kinfitstatus==0)
+    // if (kinfitstatus==0)
     //      myha.mzz = HZZRefitMass;
     //    LD_.setMeasurables(myha);
     //    float signProb = LD_.getSignalProbability();
@@ -328,6 +328,19 @@ void Higgs2l2bUserData::produce( Event & evt, const EventSetup & ) {
     h.addUserFloat("phiNT1",phiNT1);
     h.addUserFloat("phiNT2",phiNT2);
     h.addUserFloat("costhetastarNT",costhetastarNT);
+    h.addUserFloat("j1RefitPt", j1RefitPt);
+    h.addUserFloat("j2RefitPt", j2RefitPt);
+    h.addUserFloat("j1RefitEta", j1RefitEta);
+    h.addUserFloat("j2RefitEta", j2RefitEta);
+    h.addUserFloat("j1RefitPhi", j1RefitPhi);
+    h.addUserFloat("j2RefitPhi", j2RefitPhi);
+    h.addUserFloat("j1RefitE", j1RefitE);
+    h.addUserFloat("j2RefitE", j2RefitE);
+    h.addUserFloat("ZjjRefitMass", ZjjRefitMass);
+    h.addUserFloat("HZZRefitMass", HZZRefitMass);
+    h.addUserFloat("KFchiSquare", KFchiSquare);
+    h.addUserFloat("KFchiSquareProb", KFchiSquareProb);
+    //    h.addUserFloat("helyLD", helyLD);
 
 
     higgsColl->push_back(h);
@@ -425,6 +438,31 @@ void Higgs2l2bUserData::helicityAngles (const pat::CompositeCandidate &X, const 
 
 }
 
+int Higgs2l2bUserData::runJetKinFit(TLorentzVector &j1,TLorentzVector &j2,
+				    const TLorentzVector &ZLL, TLorentzVector & Zjj, 
+				    TLorentzVector &XZZ, float & chiSquare, 
+				    float & chiSquareProb) {
+  
+  int status=0;
+
+  //pass the two four momenta and initialize the Kinfit object
+  kinFitter_.setJet4Mom(j1,j2);
+
+  //ask the kinfit object to correct the four momenta
+  status=kinFitter_.Refit();
+  if(status==0){
+    j1=kinFitter_.getCorrJets().at(0);
+    j2=kinFitter_.getCorrJets().at(1);
+    chiSquare     = kinFitter_.chiSquare();
+    chiSquareProb = kinFitter_.chiSquareProb();
+  }
+
+  //update also 4-mom of XZZ and of ZJJ
+  Zjj = j1+j2;
+  XZZ = ZLL+Zjj;
+
+  return status;
+}
 
 
 #include "FWCore/Framework/interface/MakerMacros.h"
