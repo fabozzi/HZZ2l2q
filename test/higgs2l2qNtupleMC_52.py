4b19173aa0a89f53c57e7c237d602b8a64233047
fabozzi <>
2012-06-24 15:52:06 +0000

    updated event filters in 52

--- test/higgs2l2qNtupleMC_52.py
+++ test/higgs2l2qNtupleMC_52.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("TRIM")
 
-applyFilter = True
+applyFilter = False
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
@@ -20,7 +20,7 @@ process.source.fileNames=cms.untracked.vstring(
 )
 
 
-#### Event cleaning 
+##### Event cleaning 
 process.badEventFilter = cms.EDFilter(
     "HLTHighLevel",
     TriggerResultsTag = cms.InputTag("TriggerResults","","PAT"),
@@ -29,7 +29,9 @@ process.badEventFilter = cms.EDFilter(
 #                           'EcalDeadCellTriggerPrimitiveFilterPath',
                            'EcalDeadCellBoundaryEnergyFilterPath',
                            'noscrapingFilterPath',          
-                           'hcalLaserEventFilterPath',
+#                           'hcalLaserEventFilterPath',
+#### Use hcalLaserFilterFromAODPath in 5_2
+                           'hcalLaserFilterFromAODPath',
                            'HBHENoiseFilterPath'#,
 #                           'totalKinematicsFilterPath' #only for Madgraph MC
                            ),
@@ -44,51 +46,6 @@ process.PUInfoNtuple = cms.EDProducer(
     "GenPUNtupleDump",
     isData = cms.bool(False)
 )
-
-#process.HLTPassInfo = cms.EDProducer(
-#    "HLTPassInfoProducer",
-#    triggerEvent = cms.InputTag("patTriggerEvent"),
-#    # here the 1st run with a new trigger table
-#    # leave empty for MC
-#    runLimits = cms.vint32(),
-#    # here insert the HLT path (without _v[n] suffix) you want to check
-#    # MC path
-#    triggerNamesSingleMu_MC = cms.vstring(),
-#    triggerNamesDoubleMu_MC = cms.vstring(),
-#    triggerNamesSingleEl_MC = cms.vstring(),
-#    triggerNamesDoubleEl_MC = cms.vstring(),
-#    # Data: here all the paths making the PDs are listed
-#    # 5e32 paths
-#    triggerNamesSingleMu_5e32 = cms.vstring(),
-#    triggerNamesDoubleMu_5e32 = cms.vstring(),
-#    triggerNamesSingleEl_5e32 = cms.vstring(),
-#    triggerNamesDoubleEl_5e32 = cms.vstring(),
-#    # 1e33 paths
-#    triggerNamesSingleMu_1e33 = cms.vstring(),
-#    triggerNamesDoubleMu_1e33 = cms.vstring(),
-#    triggerNamesSingleEl_1e33 = cms.vstring(),
-#    triggerNamesDoubleEl_1e33 = cms.vstring(),
-###### 1.4e33 paths
-#    triggerNamesSingleMu_1p4e33 = cms.vstring(),
-#    triggerNamesDoubleMu_1p4e33 = cms.vstring(),
-#    triggerNamesSingleEl_1p4e33 = cms.vstring(),
-#    triggerNamesDoubleEl_1p4e33 = cms.vstring(),
-###### 2e33 paths
-#    triggerNamesSingleMu_2e33 = cms.vstring(),
-#    triggerNamesDoubleMu_2e33 = cms.vstring(),
-#    triggerNamesSingleEl_2e33 = cms.vstring(),
-#    triggerNamesDoubleEl_2e33 = cms.vstring(),
-###### 3e33 paths
-#    triggerNamesSingleMu_3e33 = cms.vstring(),
-#    triggerNamesDoubleMu_3e33 = cms.vstring(),
-#    triggerNamesSingleEl_3e33 = cms.vstring(),
-#    triggerNamesDoubleEl_3e33 = cms.vstring(),
-###### 5e33 paths
-#    triggerNamesSingleMu_5e33 = cms.vstring(),
-#    triggerNamesDoubleMu_5e33 = cms.vstring(),
-#    triggerNamesSingleEl_5e33 = cms.vstring(),
-#    triggerNamesDoubleEl_5e33 = cms.vstring()
-#    )
 
 # Event rho dumper
 process.rhoDumper = cms.EDProducer("EventRhoDumper",
@@ -105,7 +62,6 @@ process.metInfoProducer = cms.EDProducer("MetVariablesProducer",
 process.edmNtuplesOut.fileName = cms.untracked.string('h2l2q_ntuple.root')
 process.edmNtuplesOut.outputCommands = cms.untracked.vstring(
     "drop *",
-#    "keep *_HLTPassInfo_*_*",
     "keep *_eventVtxInfoNtuple_*_*",
     "keep *_PUInfoNtuple_*_*",
     "keep *_rhoDumper_*_*",
